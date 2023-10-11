93a4eaf49930c1cd36e7b3ad8003a4e52dcd85c0
Josep Sendra Serra <je.sendra@vewtech.net>
2023-10-11 17:37:43 +0200

    Added main components

--- ui-react/src/index.tsx
+++ ui-react/src/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import MainLayout from './layouts/MainLayout';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFoundPage from './pages/NotFoundPage';
+import MySchedulesPage from './pages/MySchedulesPage';
+import GitHubPage from './pages/GitHubPage';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <>
-  REACT UI
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<MainLayout />}>
+
+        <Route path="schedules" element={<MySchedulesPage />} />
+        <Route path='repo' element={<GitHubPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
